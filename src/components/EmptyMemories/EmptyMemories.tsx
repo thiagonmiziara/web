de272aac9e23d00df5afce2316a65e8259b2ebc9
Thiago Miziara <thiago.miziara@luizalabs.com>
2023-05-18 08:45:57 -0300

    separando components

---
+++ src/components/EmptyMemories/EmptyMemories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmptyMemories = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, começa{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
+
+export default EmptyMemories
