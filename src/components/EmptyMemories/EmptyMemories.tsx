6b4d09d9805690e90e87165711491e7b39451a63
thiagonmiziara <thiagonmiziara@gmail.com>
2023-05-20 17:15:29 -0300

    finalizando app

--- src/components/EmptyMemories/EmptyMemories.tsx
+++ src/components/EmptyMemories/EmptyMemories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EmptyMemories = () => {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, começa{' '}
         <a href="" className="underline hover:text-gray-50">
